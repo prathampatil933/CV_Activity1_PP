e2efb1422498a383120abd7ebf3ec0e619191222
Patil Pratham Narendra <pratham.patil@mitaoe.ac.in>
2023-11-01 16:28:25 +0530

    Update myscript.py

--- myscript.py
+++ myscript.py
@@ -1,41 +1,41 @@
+%%writefile my_streamlit_app.py
+# Paste your Streamlit code here
+
+
 import cv2
 import numpy as np
+import streamlit as st
 
 def main():
-    # Input image file path
-    image_path = "input2.jpg"  # Provide the path to your image file
+    st.title("Image Transformations App")
 
-    # Load the image
-    image = cv2.imread(image_path, 0)
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    if image is None:
-        print('Image not found.')
-        return
+    if uploaded_image is not None:
+        image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
+        st.image(image, caption="Original Image", use_column_width=True)
 
-    # Display the original image
-    cv2.imshow('Original Image', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        transformation_option = st.selectbox("Select Transformation", ["Translate", "Rotate", "Scale", "Shear"])
 
-    # Define transformation parameters
-    translation_matrix = np.float32([[1, 0, 50], [0, 1, 30]])  # Translate 50 pixels right and 30 pixels down
-    rotation_angle = 45  # Rotate by 45 degrees
-    scaling_factors = (2, 2)  # Scale by a factor of 2 in both x and y directions
-    shearing_matrix = np.float32([[1, 0.5, 0], [0.5, 1, 0]])  # Shear in the x-direction
+        if transformation_option == "Translate":
+            translate_x = st.slider("Translate X", -100, 100, 0)
+            translate_y = st.slider("Translate Y", -100, 100, 0)
+            translation_matrix = np.float32([[1, 0, translate_x], [0, 1, translate_y]])
+            transformed_image = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]))
+        elif transformation_option == "Rotate":
+            rotation_angle = st.slider("Rotation Angle", -180, 180, 0)
+            rotation_matrix = cv2.getRotationMatrix2D((image.shape[1] / 2, image.shape[0] / 2), rotation_angle, 1)
+            transformed_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
+        elif transformation_option == "Scale":
+            scaling_factor = st.slider("Scale Factor", 0.1, 3.0, 1.0, 0.1)
+            transformed_image = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor)
+        elif transformation_option == "Shear":
+            shear_x = st.slider("Shear X", -1.0, 1.0, 0.0, 0.01)
+            shear_y = st.slider("Shear Y", -1.0, 1.0, 0.0, 0.01)
+            shearing_matrix = np.float32([[1, shear_x, 0], [shear_y, 1, 0]])
+            transformed_image = cv2.warpAffine(image, shearing_matrix, (image.shape[1], image.shape[0]))
 
-    # Apply the transformations
-    translated_image = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]))
-    rotated_image = cv2.warpAffine(image, cv2.getRotationMatrix2D((image.shape[1] / 2, image.shape[0] / 2), rotation_angle, 1), (image.shape[1], image.shape[0]))
-    scaled_image = cv2.resize(image, None, fx=scaling_factors[0], fy=scaling_factors[1])
-    sheared_image = cv2.warpAffine(image, shearing_matrix, (image.shape[1], image.shape[0]))
-
-    # Display the transformed images
-    cv2.imshow('Translated Image', translated_image)
-    cv2.imshow('Rotated Image', rotated_image)
-    cv2.imshow('Scaled Image', scaled_image)
-    cv2.imshow('Sheared Image', sheared_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        st.image(transformed_image, caption="Transformed Image", use_column_width=True)
 
 if __name__ == "__main__":
     main()
